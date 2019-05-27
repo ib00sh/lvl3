10329d87d2a63ba0b2d9e1a533f74e7efcd4147d
max bushuev <wowmaxwow98@gmail.com>
2019-05-27 10:03:03 +0300

    Изменен строковый массив

--- src/Task2/ArrayList1.java
+++ src/Task2/ArrayList1.java
@@ -8,7 +8,7 @@ public class ArrayList1 {
     public static void main(String[] args) {
         Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         Double[] doubArray = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
-        String[] strArray = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        String[] strArray = {"один", "два", "три", "четыре", "пять", "шесть"};
         arrayToArrayList(intArray);
         arrayToArrayList(doubArray);
         arrayToArrayList(strArray);
