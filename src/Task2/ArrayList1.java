463770b3def8d98780c220f48095d96f5481aba1
max bushuev <wowmaxwow98@gmail.com>
2019-05-27 09:58:57 +0300

    Готовы 2 задачи из 3х

---
+++ src/Task2/ArrayList1.java
@@ -0,0 +1,23 @@
+package Task2;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        Double[] doubArray = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+        String[] strArray = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        arrayToArrayList(intArray);
+        arrayToArrayList(doubArray);
+        arrayToArrayList(strArray);
+        System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(doubArray));
+        System.out.println(Arrays.toString(strArray));
+    }
+
+    private static <T> ArrayList arrayToArrayList(T[] array) {
+        return new ArrayList<>(Arrays.asList(array));
+    }
+}
---
+++ src/Task_1/Array.java
@@ -0,0 +1,36 @@
+package Task_1;
+//1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        Integer[] intMass = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Double[] doubMass = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+        String[] strMass = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+        showResult(intMass,4,9);
+        showResult(doubMass,5,7);
+        showResult(strMass,2,8);
+
+    }
+
+
+    public static void sort(Object[] mass, int firstEl, int secondEl)  {
+        Object temp = mass[firstEl];
+        mass[firstEl] = mass[secondEl];
+        mass[secondEl] = temp;
+    }
+
+    public static void showResult(Object[] mass, int firstEl, int secondEl)  {
+        System.out.println(Arrays.toString(mass));
+        sort(mass, firstEl, secondEl);
+        System.out.println("Меняем местам элемент " + (firstEl+1) + " с элементом " + (secondEl + 1));
+        System.out.println("new");
+        System.out.println(Arrays.toString(mass));
+        System.out.println("______________");
+
+    }
+}
+
+
